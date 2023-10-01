78a7fb22d2e221b776272ab8a9d1867334321494
Tuldrum <andresdmunoz@unicauca.edu.co>
2023-09-30 22:49:06 -0500

    FastaDownloader

--- app/FASTAhandler.py
+++ app/FASTAhandler.py
@@ -1,39 +1,38 @@
 import requests
-
-# Replace this URL with the URL of the FASTA file you want to download
-fasta_url = "https://example.com/path/to/your.fasta"
-
-# Specify the local file path where you want to save the downloaded FASTA file
-local_file_path = "downloaded.fasta"
-
-try:
-    response = requests.get(fasta_url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Open the local file for writing in binary mode
-        with open(local_file_path, "wb") as fasta_file:
-            # Write the content of the HTTP response to the local file
-            fasta_file.write(response.content)
-        print(f"FASTA file downloaded and saved to {local_file_path}")
-    else:
-        print(f"Failed to download FASTA file. HTTP status code: {response.status_code}")
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
-
-
-
-
-"""
-
- http://artemisa.unicauca.edu.co/~eumartinez/Proteinas/uniprotkb_taxonomy_id_137071_2023_09_13.fasta
-
-
 from Bio import SeqIO
+import os 
 
-for index,record in enumerate(SeqIO.parse("uniprotkb_taxonomy_id_137071_2023_09_13.fasta", "fasta")):
-    print(record.id)
-    print(repr(record.seq))
-    print(len(record))
+class FastaDownloader: 
 
-"""
+    def __init__(self, fasta_url:str, 
+                 local_file_path:str = ".\\app\\resources\\downloaded.fasta") -> None:
+        self.fasta_url = fasta_url 
+        self.local_file_path = local_file_path
+    
+    def __download_fasta_file(self): 
+        downloaded: bool = False 
+        try:
+            response = requests.get(self.fasta_url)
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Open the local file for writing in binary mode
+                with open(self.local_file_path, "wb") as fasta_file:
+                    # Write the content of the HTTP response to the local file
+                    fasta_file.write(response.content)
+                downloaded = True 
+                print(f"FASTA file downloaded and saved to {self.local_file_path}")
+            else:
+                print(f"Failed to download FASTA file. HTTP status code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+        return downloaded 
+
+    def get_fasta_records(self):
+        if self.__download_fasta_file():
+            if not os.path.isfile(self.local_file_path):
+                raise FileNotFoundError(f"The downloaded file {self.local_file_path} does not exist.")
+            
+            return list(SeqIO.parse(self.local_file_path, "fasta"))
+        else:
+            raise Exception("No se pudo descargar el archivo")
--- app/principal.py
+++ app/principal.py
@@ -1,4 +1,14 @@
 from plano import SequenceProcessor 
+from FASTAhandler import FastaDownloader 
 
 if __name__ == '__main__':
-    processor = SequenceProcessor(kmerSize=4, sequence= "")
+
+    fasta_url = "http://artemisa.unicauca.edu.co/~eumartinez/Proteinas/uniprotkb_taxonomy_id_137071_2023_09_13.fasta" 
+    local_file_path = "ruta_local_del_archivo.fasta"
+    fasta_downloader = FastaDownloader(fasta_url)
+    records = fasta_downloader.get_fasta_records()
+
+    record = records[0]
+    print(record)
+    processor = SequenceProcessor(kmerSize=4, sequence=str(record.seq))
+    processor.getRepeats() 
